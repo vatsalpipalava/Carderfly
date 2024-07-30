1d2234623381cc5b397655e45b891010edc17b38
Vatsal Pipalava <vatsalpipalava@gmail.com>
2024-07-30 14:59:20 +0530

    index path change

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-import { app } from "./app.js";
+import { app } from "./src/app.js";
 import dotenv from "dotenv";
-import connectDB from "./db/index.js";
+import connectDB from "./src/db/index.js";
 
 dotenv.config({ path: "./.env" });
 
