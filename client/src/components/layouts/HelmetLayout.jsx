03305f63d456548c5caa86ed2514347883117348
Vatsal Pipalava <vatsalpipalava@gmail.com>
2024-08-09 11:52:25 +0530

    image add to og image

--- client/src/components/layouts/HelmetLayout.jsx
+++ client/src/components/layouts/HelmetLayout.jsx
@@ -10,11 +10,26 @@ function HelmetLayout({ title, description, imageUrl, pageUrl, children }) {
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={imageUrl} />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="350" />
+        <meta property="og:image:height" content="350" />
         <meta property="og:url" content={pageUrl} />
         <meta property="og:type" content="website" />
+
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={imageUrl} />
+
+        <script type="application/ld+json">
+          {`
+            {
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "url": ${pageUrl},
+              "logo": ${imageUrl}
+            }
+          `}
+        </script>
       </Helmet>
       {children}
     </>
