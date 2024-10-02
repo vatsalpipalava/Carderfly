ad7c3c8940a565039d17010144899439e4369d81
Vatsal Pipalava <vatsalpipalava@gmail.com>
2024-10-02 06:41:31 +0000

    allowo origin update

--- server/src/config/allowedOrigins.js
+++ server/src/config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
-  process.env.FIS_FRONTEND_URL,
-  process.env.SEC_FRONTEND_URL,
-  process.env.FIS_ADMIN_URL,
-  process.env.SEC_ADMIN_URL,
+  `${process.env.FIS_FRONTEND_URL}`,
+  `${process.env.SEC_FRONTEND_URL}`,
+  `${process.env.FIS_ADMIN_URL}`,
+  `${process.env.SEC_ADMIN_URL}`,
 ];
 
 export { allowedOrigins };
